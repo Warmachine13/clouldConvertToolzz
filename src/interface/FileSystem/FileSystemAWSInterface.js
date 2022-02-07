1155513031b6ed455ae55de0a64f9515d026500f
Lucas Pereira <lucaspereiradesouzat@gmail.com>
2022-02-07 17:39:20 -0300

    fix pdf ContentType

--- src/interface/FileSystem/FileSystemAWSInterface.js
+++ src/interface/FileSystem/FileSystemAWSInterface.js
@@ -19,10 +19,12 @@ class FileSystemAWSInterface {
       Body: data,
       Key: `convery/${Date.now()}.pdf`,
       ACL: "public-read",
+      ContentDisposition: "inline",
+      ContentType: "application/pdf",
     };
 
     return await (
-      await this.s3.upload(uploadParams).promise()
+      await this.s3.upload({ uploadParams }).promise()
     ).Location;
   }
 }
